17a85c79e575c0495d8f08cdba64b22554a11796
h-bahrami <h.bahrami@live.com>
2021-07-07 16:08:31 +0200

    prettier

--- src/AppEvents.tsx
+++ src/AppEvents.tsx
@@ -2,22 +2,19 @@ import { Component, ReactNode } from "react";
 
 import { AppEventsContainerProps } from "../typings/AppEventsProps";
 
-
 export default class Events extends Component<AppEventsContainerProps> {
     private isExecuted = false;
     render(): ReactNode {
-        
-        if (!this.isExecuted &&  this.props.onLoadAction && this.props.onLoadAction.canExecute){
+        if (!this.isExecuted && this.props.onLoadAction && this.props.onLoadAction.canExecute) {
             this.isExecuted = true;
             this.props.onLoadAction.execute();
         }
         return null;
     }
     componentWillUnmount(): void {
-      
         if (this.props.onUnloadAction && this.props.onUnloadAction.canExecute) {
-             this.props.onUnloadAction.execute();
-        }        
+            this.props.onUnloadAction.execute();
+        }
     }
 
     componentDidMount(): void {
@@ -27,9 +24,8 @@ export default class Events extends Component<AppEventsContainerProps> {
         //     if (this.props.onLoadAction && this.props.onLoadAction.canExecute) {
         //         // console.log('onLoadAction is not null and can be executed');
         //         this.props.onLoadAction.execute();
-        //     }    
+        //     }
         // }, 500);
-        
     }
 
     // UNSAFE_componentWillMount(): void {
